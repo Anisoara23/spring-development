c8775e609c2cd70514edc8e8f596cc6b04cc9646
Anisoara Vlasi <anisoara.vlasi@iongroup.com>
2024-02-21 14:50:09 +0200

    Add spring-boot-starter-data-rest for HTEAOS and spring-data-rest-hal-explorer for browser explorer

--- microservice/src/main/java/com/example/repo/TourPackageRepository.java
+++ microservice/src/main/java/com/example/repo/TourPackageRepository.java
@@ -2,9 +2,11 @@ package com.example.repo;
 
 import com.example.domain.TourPackage;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Optional;
 
+@RepositoryRestResource(collectionResourceRel = "packages", path = "packages")
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
     Optional<TourPackage> findByName(String tourPackageName);
 }
--- microservice/src/main/java/com/example/repo/TourRepository.java
+++ microservice/src/main/java/com/example/repo/TourRepository.java
@@ -1,7 +1,21 @@
 package com.example.repo;
 
 import com.example.domain.Tour;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
-public interface TourRepository extends CrudRepository<Tour, Integer> {
+import java.util.List;
+
+public interface TourRepository extends CrudRepository<Tour, Integer>, PagingAndSortingRepository<Tour, Integer> {
+
+    Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);
+
+    @RestResource(exported = false)
+    @Override
+    <S extends Tour> S save(S entity);
 }
