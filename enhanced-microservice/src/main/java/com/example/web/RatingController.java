e76b159acbbd83894dc70c004230682c1335d7e8
Anisoara Vlasi <anisoara.vlasi@iongroup.com>
2024-02-23 09:50:07 +0200

    Add microservice-enhanced package with Tour California application enhanced by using sql files for generating schema and add data to db and by implementing HTEOS

---
+++ enhanced-microservice/src/main/java/com/example/web/RatingController.java
@@ -0,0 +1,52 @@
+package com.example.web;
+
+
+import com.example.service.TourRatingService;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping(path = "/ratings")
+public class RatingController {
+    private final TourRatingService tourRatingService;
+
+    private final RatingAssembler assembler;
+
+    public RatingController(TourRatingService tourRatingService, RatingAssembler assembler) {
+        this.tourRatingService = tourRatingService;
+        this.assembler = assembler;
+    }
+
+    @GetMapping
+    public CollectionModel<RatingDto> getAll() {
+        return assembler.toCollectionModel(tourRatingService.lookupAll());
+    }
+
+    @GetMapping("/{id}")
+    public RatingDto getRating(@PathVariable("id") Integer id) {
+        return assembler.toModel(tourRatingService.lookupRatingById(id)
+                .orElseThrow(() -> new NoSuchElementException("Rating " + id + " not found"))
+        );
+    }
+
+
+    /**
+     * Exception handler if NoSuchElementException is thrown in this Controller
+     *
+     * @param ex exception
+     * @return Error message String.
+     */
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NoSuchElementException.class)
+    public String return400(NoSuchElementException ex) {
+        return ex.getMessage();
+    }
+}
---
+++ enhanced-microservice/src/main/java/com/example/web/TourRatingController.java
@@ -0,0 +1,150 @@
+package com.example.web;
+
+
+import com.example.domain.TourRating;
+import com.example.service.TourRatingService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.AbstractMap;
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping(path = "/tours/{tourId}/ratings")
+public class TourRatingController {
+    private final TourRatingService tourRatingService;
+    private final RatingAssembler assembler;
+
+    protected TourRatingController(TourRatingService tourRatingService, RatingAssembler assembler) {
+        this.tourRatingService = tourRatingService;
+        this.assembler = assembler;
+    }
+
+    /**
+     * Create a Tour Rating.
+     *
+     * @param tourId
+     * @param ratingDto
+     */
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createTourRating(@PathVariable(value = "tourId") int tourId, @RequestBody @Validated RatingDto ratingDto) {
+        tourRatingService.createNew(tourId, ratingDto.getCustomerId(), ratingDto.getScore(), ratingDto.getComment());
+    }
+
+    /**
+     * Create Several Tour Ratings for one tour, score and several customers.
+     *
+     * @param tourId
+     * @param score
+     * @param customers
+     */
+    @PostMapping("/{score}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createManyTourRatings(@PathVariable(value = "tourId") int tourId,
+                                      @PathVariable(value = "score") int score,
+                                      @RequestParam("customers") Integer customers[]) {
+        tourRatingService.rateMany(tourId, score, customers);
+    }
+
+    /**
+     * Lookup a the Ratings for a tour.
+     *
+     * @param tourId
+     * @param pageable
+     * @return
+     */
+    @GetMapping
+    public PagedModel<RatingDto> getAllRatingsForTour(@PathVariable(value = "tourId") int tourId, Pageable pageable,
+                                                      PagedResourcesAssembler pagedAssembler) {
+        Page<TourRating> tourRatingPage = tourRatingService.lookupRatings(tourId, pageable);
+        return pagedAssembler.toModel(tourRatingPage, assembler);
+    }
+
+    /**
+     * Calculate the average Score of a Tour.
+     *
+     * @param tourId
+     * @return Tuple of "average" and the average value.
+     */
+    @GetMapping("/average")
+    public AbstractMap.SimpleEntry<String, Double> getAverage(@PathVariable(value = "tourId") int tourId) {
+        return new AbstractMap.SimpleEntry<String, Double>("average", tourRatingService.getAverageScore(tourId));
+    }
+
+    /**
+     * Update score and comment of a Tour Rating
+     *
+     * @param tourId
+     * @param ratingDto
+     * @return The modified Rating DTO.
+     */
+    @PutMapping
+    public RatingDto updateWithPut(@PathVariable(value = "tourId") int tourId, @RequestBody @Validated RatingDto ratingDto) {
+        return toDto(tourRatingService.update(tourId, ratingDto.getCustomerId(),
+                ratingDto.getScore(), ratingDto.getComment()));
+    }
+    /**
+     * Update score or comment of a Tour Rating
+     *
+     * @param tourId
+     * @param ratingDto
+     * @return The modified Rating DTO.
+     */
+    @PatchMapping
+    public RatingDto updateWithPatch(@PathVariable(value = "tourId") int tourId, @RequestBody @Validated RatingDto ratingDto) {
+        return toDto(tourRatingService.updateSome(tourId, ratingDto.getCustomerId(),
+                ratingDto.getScore(), ratingDto.getComment()));
+    }
+
+    /**
+     * Delete a Rating of a tour made by a customer
+     *
+     * @param tourId
+     * @param customerId
+     */
+    @DeleteMapping("/{customerId}")
+    public void delete(@PathVariable(value = "tourId") int tourId, @PathVariable(value = "customerId") int customerId) {
+        tourRatingService.delete(tourId, customerId);
+    }
+
+    /**
+     * Convert the TourRating entity to a RatingDto
+     *
+     * @param tourRating
+     * @return RatingDto
+     */
+    private RatingDto toDto(TourRating tourRating) {
+        return new RatingDto(tourRating.getScore(), tourRating.getComment(), tourRating.getCustomerId());
+    }
+
+    /**
+     * Exception handler if NoSuchElementException is thrown in this Controller
+     *
+     * @param ex
+     * @return Error message String.
+     */
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NoSuchElementException.class)
+    public String return400(NoSuchElementException ex) {
+        return ex.getMessage();
+
+    }
+
+}
