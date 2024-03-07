302d5566c3b2df93a71b312648522c508ea5acb4
Anisoara Vlasi <anisoara.vlasi@iongroup.com>
2024-03-07 14:34:12 +0200

    Add basic auth

--- security/src/main/java/org/example/config/SecurityConfig.java
+++ security/src/main/java/org/example/config/SecurityConfig.java
@@ -1,8 +1,52 @@
 package org.example.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
+    public SecurityFilterChain webSecurityConfiguration(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.authorizeHttpRequests(cust ->
+                        cust.requestMatchers("/", "/home").permitAll()
+                                .anyRequest()
+                                .authenticated()).csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(cust -> cust.init(httpSecurity))
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService service() {
+        UserDetails userDetails = User
+                .withUsername("user")
+                .password("pass")
+                .passwordEncoder(pass -> passwordEncoder().encode(pass))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
